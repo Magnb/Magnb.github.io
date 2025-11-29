d43a80aec9b5dee1617c562a4e467b59b6d238ae
Magda <m.breitenauer@outlook.com>
2025-11-29 21:37:03 +0100

    published time

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764444186',resources: [
+    cacheName: 'chirpy-1764448561',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
